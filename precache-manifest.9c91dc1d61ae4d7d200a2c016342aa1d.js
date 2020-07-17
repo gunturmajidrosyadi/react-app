819ccfb7d8f33b2b63f022e8fbab070b50981dd4
Guntur Majid Rosyadi <gunturmajidrosyadi@gmail.com>
2020-07-18 06:53:10 +0700

    Updates

---
+++ precache-manifest.9c91dc1d61ae4d7d200a2c016342aa1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbcd968d993cd1a5f8496e77ace876c",
+    "url": "/gunturmajidrosyadi/hakita-web.git/index.html"
+  },
+  {
+    "revision": "c0b6dd6ff353ef79a42a",
+    "url": "/gunturmajidrosyadi/hakita-web.git/static/css/main.48bdbc6b.chunk.css"
+  },
+  {
+    "revision": "751e5b10d2bc8f1c03c7",
+    "url": "/gunturmajidrosyadi/hakita-web.git/static/js/2.8440a7d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/gunturmajidrosyadi/hakita-web.git/static/js/2.8440a7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b6dd6ff353ef79a42a",
+    "url": "/gunturmajidrosyadi/hakita-web.git/static/js/main.3409ad78.chunk.js"
+  },
+  {
+    "revision": "0c5f5d4ad2fd42c5f922",
+    "url": "/gunturmajidrosyadi/hakita-web.git/static/js/runtime-main.c1cccdce.js"
+  }
+]);
